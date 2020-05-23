30cc7076dbbac17d90a20835dfb13d841f0aaed0
jamhyunsang <gussang95@gmail.com>
2020-05-24 01:34:21 +0900

    2. 트랜스폼
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject go_camera;
+
+
+    void Update()
+    {
+        transform.RotateAround(go_camera.transform.position, Vector3.up, 100 * Time.deltaTime);
+
+    }
+}
