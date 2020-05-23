5d58c294cbee7d86a906fa431ccc9f4b42a09c03
jamhyunsang <gussang95@gmail.com>
2020-05-24 04:27:03 +0900

    8. 애니메이션
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Secne8.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secne8 : MonoBehaviour
+{
+    private Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            anim.Blend("Scene8-1");
+        }
+    }
+}
