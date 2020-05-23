248a299acb3ca3e8c39c62c574712410b98e1936
jamhyunsang <gussang95@gmail.com>
2020-05-24 02:09:57 +0900

    6. 카메라
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene6.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene6 : MonoBehaviour
+{
+    // [SerializeField]
+    // private GameObject go_Target;
+    // [SerializeField]
+    // private float speed;
+    //
+    // private Vector3 difValue;
+
+    private Camera theCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //  difValue = transform.position - go_Target.transform.position;
+    //  difValue = new Vector3(Mathf.Abs(difValue.x), Mathf.Abs(difValue.y), Mathf.Abs(difValue.z));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position= Vector3.Lerp(this.transform.position,go_Target.transform.position+difValue,speed);        
+    }
+}
