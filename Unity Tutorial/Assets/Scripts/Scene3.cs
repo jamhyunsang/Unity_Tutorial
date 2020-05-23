7051a89be48295f4ae0e1271c1104659e2d6899a
jamhyunsang <gussang95@gmail.com>
2020-05-24 01:34:57 +0900

    3. 리지드 바디
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene3 : MonoBehaviour
+{
+    private Rigidbody myRigid;
+    private Vector3 rotation;
+     // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //myRigid.velocity = Vector3.forward;
+            //myRigid.angularVelocity = Vector3.right*100;
+            //myRigid.maxAngularVelocity = 100;
+            //rotation += new Vector3(90,0,0) * Time.deltaTime;
+            //myRigid.MoveRotation (Quaternion.Euler(rotation));
+            //myRigid.AddForce(Vector3.forward);
+            //myRigid.AddTorque(Vector3.up);
+            myRigid.AddExplosionForce(10,this.transform.right,10);
+        }
+    }
+}
