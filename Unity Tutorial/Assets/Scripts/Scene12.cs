9d1e4454a0e1cce9451ca5b2864ec3fe1220dc39
jamhyunsang <gussang95@gmail.com>
2020-05-24 22:12:29 +0900

    12.슬라이더 UI
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene12.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene12 : MonoBehaviour
+{
+
+    [SerializeField] private Slider slider;
+    private bool isClick=false;
+    private float dotTime = 1f;
+    private float currentDotTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDotTime = dotTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isClick)
+        {
+            currentDotTime -= Time.deltaTime;
+
+            if(currentDotTime<=0)
+            {
+                slider.value -= Time.deltaTime;
+
+                if(currentDotTime<=-1f)
+                {
+                    currentDotTime = dotTime;
+                }
+            }
+
+        }
+    }
+
+    public void  Button()
+    {
+        isClick = true;
+    }
+}
