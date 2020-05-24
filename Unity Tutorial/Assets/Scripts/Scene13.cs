0240731168445065b0d6c6f98f99b2d86f59ca1f
jamhyunsang <gussang95@gmail.com>
2020-05-24 23:28:44 +0900

    13. 인풋 필드
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene13.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene13 : MonoBehaviour
+{
+    [SerializeField] private InputField inmoney;
+    [SerializeField] private Text outmoney2;
+    // Start is called before the first frame update
+
+    private int currentMoney = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Input()
+    {
+        currentMoney += int.Parse(inmoney.text);
+        outmoney2.text = currentMoney.ToString();
+    }
+    public void Output()
+    {
+        currentMoney -= int.Parse(inmoney.text);
+        outmoney2.text = currentMoney.ToString();
+    }
+}
