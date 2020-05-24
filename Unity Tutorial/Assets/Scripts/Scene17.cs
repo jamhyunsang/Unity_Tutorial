24d399c974af4a38b96b5540b877f5a3fb7150e5
jamhyunsang <gussang95@gmail.com>
2020-05-25 02:10:21 +0900

    17. physics 클래스
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene17.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene17 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(this.transform.position,this.transform.forward,out hitInfo,10f))
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
