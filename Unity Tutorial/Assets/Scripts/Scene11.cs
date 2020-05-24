f086e85b22920fb64b57971e92b80d9148a492be
jamhyunsang <gussang95@gmail.com>
2020-05-24 21:47:27 +0900

    11.UI의 기본
    -유니티버젼 : Unity 2019.3.13f1 (64-bit)

---
+++ Unity Tutorial/Assets/Scripts/Scene11.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene11 : MonoBehaviour
+{
+    [SerializeField] private Text txt_name;
+    [SerializeField] private Image img_name;
+
+    private bool iscooltime = false;
+
+    private float currentTime = 5f;
+    private float delayTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(iscooltime)
+        {
+            currentTime -= Time.deltaTime;
+            img_name.fillAmount = currentTime/delayTime;
+            if(currentTime<=0)
+            {
+                iscooltime = false;
+                currentTime = delayTime;
+                img_name.fillAmount = currentTime;
+            }
+        }
+    }
+
+    public void Change()
+    {
+        txt_name.text = "변경됨";
+        iscooltime = true;
+    }
+}
